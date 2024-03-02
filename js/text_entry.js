2199cefd7957a46e93bc0f55e3ee9f74dfac4f82
Luke0328 <curly0328@gmail.com>
2024-03-02 14:31:47 -0700

    Upload txt file

--- js/text_entry.js
+++ js/text_entry.js
@@ -1,9 +1,29 @@
 let text = "";
 
 function submitOnClick() {
-    text = document.getElementById('textbox').value;
+    if (text === "") {
+        text = document.getElementById('textbox').value;
+    }
     console.log(text);
 }
 
 let submitButton = document.querySelector('#submit-button');
-submitButton.addEventListener("click", submitOnClick)
+submitButton.addEventListener("click", submitOnClick);
+
+let uploadButton = document.querySelector('#upload');
+uploadButton.addEventListener("input", function() {
+    const file = this.files[0]
+  
+    let fr = new FileReader()
+    
+    fr.readAsText(file)
+    
+    fr.onload = () => {
+      console.log(fr.result)
+      text = fr.result;
+    }
+    
+    fr.onerror = () => {
+      console.log(fr.error)
+    }
+});
