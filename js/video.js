8f696d0f841c3810096cbf352bd14133dd5d8c23
dale4990 <dale4990@colorado.edu>
2024-03-02 16:45:19 -0700

    fixed i++ bug

--- js/video.js
+++ js/video.js
@@ -1,10 +1,10 @@
 let fullText = "New pipeline structure: F, DS, IS, EX, WB DS from fetch to the scoreboard (no scoreboard or WAW) -> stallIS for functional units. RAW -> read registers, go directly to EX EX: notify scoreboard when done WB: WAR hazard -> wait"
 const wordsDisplayed = 4;
-const msDelayed = 2000;
+const msDelayed = 500;
 const words = fullText.split(' ');
 
 let overlayText = document.querySelector("#overlay-text");
-for (let i = 0; i < words.length; i++) {
+for (let i = 0; i < words.length; i+=wordsDisplayed) {
     setTimeout(() => {
             let text = words.slice(i, i + wordsDisplayed).join(' ');
             overlayText.textContent = text;
