38d62c848c679ac428c81aa6a5eddc6fe09ab1fa
Luke0328 <curly0328@gmail.com>
2024-03-02 16:36:55 -0700

    Text overlay

---
+++ js/video.js
@@ -0,0 +1,15 @@
+let fullText = "New pipeline structure: F, DS, IS, EX, WB DS from fetch to the scoreboard (no scoreboard or WAW) -> stallIS for functional units. RAW -> read registers, go directly to EX EX: notify scoreboard when done WB: WAR hazard -> wait"
+const wordsDisplayed = 4;
+const msDelayed = 2000;
+const words = fullText.split(' ');
+
+let overlayText = document.querySelector("#overlay-text");
+for (let i = 0; i < words.length; i++) {
+    setTimeout(() => {
+            let text = words.slice(i, i + wordsDisplayed).join(' ');
+            overlayText.textContent = text;
+    }, i * msDelayed);
+}
+
+
+
