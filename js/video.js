8e29bdff7ad400128ff5af8f7c39bd8ba918675e
dale4990 <dale4990@colorado.edu>
2024-03-02 18:20:20 -0700

    linked text input to video

--- js/video.js
+++ js/video.js
@@ -1,4 +1,4 @@
-let fullText = "New pipeline structure: F, DS, IS, EX, WB DS from fetch to the scoreboard (no scoreboard or WAW) -> stallIS for functional units. RAW -> read registers, go directly to EX EX: notify scoreboard when done WB: WAR hazard -> wait"
+let fullText = localStorage.getItem("text")
 const wordsDisplayed = 4;
 const msDelayed = 500;
 const words = fullText.split(' ');
