25240bd85e8dc57fa9eaf085820f9fc1cffd225e
Luke0328 <curly0328@gmail.com>
2024-03-02 18:04:27 -0700

    Redirect to video

--- js/text_entry.js
+++ js/text_entry.js
@@ -1,12 +1,15 @@
+localStorage.clear();
 localStorage.setItem("text", "");
 
 function submitOnClick() {
+    event.preventDefault();
     let text = localStorage.getItem("text");
     if (text === "") {
         console.log(document.getElementById('textbox').value);
         localStorage.setItem("text", document.getElementById('textbox').value);
     }
     console.log(localStorage.getItem("text"));
+    window.location.href = "Video.html";
 }
 
 let submitButton = document.querySelector('#submit-button');
@@ -24,6 +27,7 @@ uploadButton.addEventListener("input", function() {
       console.log(fr.result)
       localStorage.setItem("text", fr.result);
       console.log(localStorage.getItem("text"));
+      window.location.href = "Video.html";
     }
     
     fr.onerror = () => {
